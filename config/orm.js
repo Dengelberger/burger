cfd8a6d4244eaec72368166119bb0071e10012f7
Donna Engelberger <dengelberger@gmail.com>
2020-10-07 21:13:32 -0400

    initial information added to files

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Ragin Cajun Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheesesteak Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quesadilla Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Almost Meat Veggie Burger');
